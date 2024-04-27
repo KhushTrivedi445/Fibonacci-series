74150cd77a91cf77ada398e357a146028f7acbf1
KhushTrivedi445 <168230450+KhushTrivedi445@users.noreply.github.com>
2024-04-28 01:15:55 +0530

    Add files via upload

---
+++ Fibonacci series in c using recursion/fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int fibo(int a);
+int main () {
+    int n;
+printf("Enter the number : ");
+scanf("%d",&n);
+
+for(int i=0;i<n;i++){
+printf ("%d",fibo(i));
+}
+
+
+
+
+    return 0;
+}
+
+
+
+int fibo(int a) {
+    if(a==0||a==1){
+        return a;
+    } else {
+        return fibo(a-1)+fibo(a-2);
+    }
+}
